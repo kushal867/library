3d455e7f58d5f13952ce99681c1e66f317119be8
Kushal <kushalsapkota577@gmail.com>
2025-12-17 17:14:04 +0545

    changes on the logics

--- librarymanagementsystem/urls.py
+++ librarymanagementsystem/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/login/', permanent=False), name='root'),
     path('', include('user.urls')),
     path('home/', include('home.urls')),
 ]
